c2ea271a3fbd4dffaec9ee72df163610b9bbda97
mekk255 <mekk2555@gmail.com>
2019-11-19 21:30:56 +0100

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args) => CallWebApi().GetAwaiter().GetResult();
+
+            static async Task CallWebApi()
+            {
+                // discover endpoints from metadata
+                var disco = await DiscoveryClient.GetAsync("https://localhost:5001");
+                if (disco.IsError)
+                {
+                    Console.WriteLine(disco.Error);
+                    return;
+                }
+
+                // request token
+                var tokenClient = new TokenClient(disco.TokenEndpoint, "consoleappclient", "secret");
+                var tokenResponse = await tokenClient.RequestClientCredentialsAsync("testapi");
+
+                if (tokenResponse.IsError)
+                {
+                    Console.WriteLine(tokenResponse.Error);
+                    return;
+                }
+
+                Console.WriteLine(tokenResponse.Json);
+
+                // call api
+                var client = new HttpClient();
+                client.SetBearerToken(tokenResponse.AccessToken);
+
+                var response = await client.GetAsync("https://localhost:5002/api/secure");
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine(response.StatusCode);
+                }
+                else
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(JArray.Parse(content));
+                }
+            }
+            
+    }
+}
---
+++ identityServerTest/AspNetCoreIdentityServer/Config.cs
@@ -0,0 +1,38 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace identityServerTest.AspNetCoreIdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "consoleappclient",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "testapi" }
+                }
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("testapi", "My test API")
+            };
+        }
+
+    }
+}
